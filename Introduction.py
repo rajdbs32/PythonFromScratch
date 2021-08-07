a9721d87a53751aa53621db920f425f2c69a5e00
RAJDEEP RAHA <52234042+rajdbs32@users.noreply.github.com>
2021-08-07 21:35:42 +0530

    all basic commands

--- Introduction.py
+++ Introduction.py
@@ -1,2 +1,21 @@
-print("Hello world\n")
+import math
+import abc
+#this symbol is used as a comment
+print (math.gcd(3,6)) #gcd =greatest common divisor.
+
+#DATATYPES IN PYTHON.
+a = "Rajdeep" #string datatype
+b = 34 #integer datatype
+c = 45.32 #float datatype
+d= 2
+#print out will all the dmas Functions.
+print(b+c) #we can do manipulation too
+print(b-c)
+print(b/d)
+print(b*d)
+
+
+
+
+
 
